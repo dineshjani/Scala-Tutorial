24b5bc9c79713ab59c5987f7bee9192de7fca657
dineshjani <dineshjani450@gmail.com>
2024-07-09 01:20:52 +0530

    three sum problem

---
+++ functional-programming-book-execise/three_sum.scala
@@ -0,0 +1,50 @@
+object Solution {
+    def threeSum(nums: Array[Int]): List[List[Int]] = {
+    def twoSum(nums: List[Int], target: Int, first: Int): List[List[Int]] = {
+    def skipLeftDuplicates(left: Int, lastValue: Int): Int = {
+      if ((left < nums.length) && (nums(left) == lastValue)) {
+          skipLeftDuplicates(left + 1, lastValue)
+      }
+      else left
+    }
+
+    def skipRightDuplicates(right: Int, lastValue: Int): Int = {
+          if (right >= 0 && nums(right) == lastValue) skipRightDuplicates(right - 1, lastValue)
+          else right
+        }
+      @annotation.tailrec
+      def loop(nums: List[Int], left: Int, right: Int, acc: List[List[Int]]): List[List[Int]] = {
+        if (left >= right) acc
+        else {
+          val sum = nums(left) + nums(right)
+          if (sum == target) {
+              val newLeft = skipLeftDuplicates(left + 1, nums(left))
+              val newRight = skipRightDuplicates(right - 1, nums(right))
+              val newAcc = acc :+ List(first, nums(left), nums(right))
+                      loop(nums, newLeft, newRight, newAcc)
+          } else if (sum < target) {
+            loop(nums, left + 1, right, acc)
+          } else {
+            loop(nums, left, right - 1, acc)
+          }
+        }
+      }
+      loop(nums, 0, nums.length - 1, List())
+    }
+
+    @annotation.tailrec
+    def threeSumRec(nums: List[Int], result: List[List[Int]]): List[List[Int]] = nums match {
+      case Nil => result
+      case head :: tail =>
+        if (result.nonEmpty && head == result.head.head) {
+          threeSumRec(tail, result)
+        } else {
+          val twoSumResult = twoSum(tail, -head, head)
+          threeSumRec(tail,  twoSumResult ++ result)
+        }
+    }
+
+    val sortedNums = nums.sorted.toList
+    threeSumRec(sortedNums, List())
+    }
+}
